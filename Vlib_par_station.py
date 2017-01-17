be9cb6b5231ce875c75a9ebbdf64e72aaf2bc72a
AdrienKr <kraych.adrien@gmail.com>
2017-01-17 19:11:03 +0100

    VlibDispo_dans_le_temps
    A l'aide de la fonction time, j'ai éssayé de tracer l'affluence d'une station durant 1 heure. Dans une boucle for je prend 60 mesure, chacune espacé de 1 minute ( time.sleep(60)). Et on trace le graphe avec les valeur ainsi collectés

--- Vlib_par_station.py
+++ Vlib_par_station.py
@@ -1,42 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 16 19:59:43 2017
+Created on Tue Jan 17 16:21:24 2017
 
-@author: ak
+@author: A.K.
 """
-
-#Le but du code est de se familiariser avec les données du site et d'éssayer
-# de commencer a les mettre en forment
 
 from __future__ import division
 import requests
 import json
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 
-#Recupération des données sur site
-r = requests.get("http://opendata.paris.fr//api/records/1.0/search?dataset=stations-velib-disponibilites-en-temps-reel")
-#Transformation en dico
-dico = r.json()
+#Vecteur temps - (chaqu minute)
+temps = np.arange(time.time(),time.time()+3600,60)
+#creation d'un vecteur qui prendera le nombre de vélo dispo
+Nb_Bike = np.ones(60)
 
-#Creation d'un ficher, pour réorganiser les données qui nous intéréssent
-f = open('NbVLib_fct_station','w')
+for n in xrange(60):
+        #Recupération des données sur site
+        r = requests.get("http://opendata.paris.fr//api/records/1.0/search?dataset=stations-velib-disponibilites-en-temps-reel")
+        #Transformation en dico
+        dico = r.json()
+        
+        
+        #Données utilisées(le nom de la station n'est pas forcément nécessaire )
+        name_station = str(dico['records'][6]['fields']['name'].encode('utf-8'))
+        Nb_Bike[n] = int(dico['records'][6]['fields']['available_bikes'])
+        
 
-#Boucle qui selectionne Nombre de Vélos disponibles par station
-for n in xrange(len(dico['records'])):
- data = [str(dico['records'][n]['fields']['name']),str(dico['records'][n]['fields']['available_bikes'])]
- f.write(data[0])
- f.write(data[1])
- f.write('\n')
-f.close() 
-
-#Commentaire: J'ai taper ce code dans le but d'éssayer d'organiser le ficher
-# recu par le site Paris Data, dans l'exemple j'ai sélectionné le nombres de
-# vélos par station et l'ai inscrit dans un fichier.
-
-#Suite possible? : Peut être allez plus loin et isoler le nombre de  vélo
-#par stations et s'amuser à tracer un graphe qui indique en fonction du temps
-# le nombre de vélo disponible pour une station choisie par l'utilisateur (en
-# réactulisant réguliérement les données du site)
- 
-
- 
+        time.sleep(60)
+        
+plt.plot(temps,Nb_Bike)
+#Commentaire : Je transformmerais prochainement le code en fonction

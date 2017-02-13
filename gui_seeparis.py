582182c6fe7dfbe7b47b07054c9aea0c95a8b404
Tiphn3 <paiik.tiphaine@gmail.com>
2017-02-13 16:18:32 +0100

    Add files via upload

--- gui_seeparis.py
+++ gui_seeparis.py
@@ -5,6 +5,7 @@ Created on Tue Feb 07 10:29:50 2017
 
 @author: Tiphaine
 """
+import os
 
 from PyQt4 import QtGui,QtCore,QtWebKit
 import pyqtgraph as pg
@@ -20,14 +21,14 @@ if APP == None:
     APP = QtGui.QApplication(["parisdata"])
     
 
-############################### Version 3 #####################################
+############################### Version 4 #####################################
 # 1) Google Maps s'ouvre dans un widget de l'interface graphique
 # 2) les donnees velib telechargees contiennent un nombre ajustable de lignes 
 # (contre 10 par defaut)
 # 4) Plusieurs boutons : arbres remarquables, cinemas, musee
 # 5) histogramme du nombre moyen de velib disponibles par arrondissement.
+# 6) histogramme du nombre moyen de velib par arrdt sur une journée
 ###############################################################################
-
 
 class SeeParisGui(QtGui.QWidget):
     def __init__(self):
@@ -38,19 +39,26 @@ class SeeParisGui(QtGui.QWidget):
         self.layout = QtGui.QGridLayout()   
         self.setLayout(self.layout)
         
+        self.v_layout = QtGui.QVBoxLayout()
+        self.layout.addLayout(self.v_layout,0,0)
+        
         # Widget internet : affiche les differentes cartes
         self.html1 = QtWebKit.QWebView()
-        self.html1.setFixedHeight(350)
+        self.html1.setFixedHeight(400)
         self.layout.addWidget(self.html1,0,1)
         
         # Widget internet : reserve au Google Maps
         self.html2 = QtWebKit.QWebView()
-        self.html2.setFixedHeight(300)
+        self.html2.setFixedHeight(400)
         self.layout.addWidget(self.html2,2,1)
         
-        # Plot Widget
-        self.plotwidget = pg.PlotWidget(title=u"Nombre moyen de velib par arrondissement")
+        # Plot Widget 1
+        self.plotwidget = pg.PlotWidget(title=u"Données courantes")
         self.layout.addWidget(self.plotwidget,0,2)
+        
+        # Plot Widget 2
+        self.plotwidget2 = pg.PlotWidget(title=u"Données journalières (11/02/2017)")
+        self.layout.addWidget(self.plotwidget2,2,2)
         
         ############################
         
@@ -63,32 +71,32 @@ class SeeParisGui(QtGui.QWidget):
         # Bouton Carte des velibs dans Paris
         self.button1 = QtGui.QPushButton(u"Carte des vélibs")
         self.button1.setFixedWidth(300)
-        self.layout.addWidget(self.button1,0,0)
+        self.v_layout.addWidget(self.button1)
         self.button1.clicked.connect(self.velib_carte)
         
         # Line Edit for method Velib_arrondissement
         self.l1 = QtGui.QLabel()
         self.l1.setFixedWidth(300)
         self.l1.setText(u"Numéro d'arrondissement")
-        self.layout.addWidget(self.l1,1,0)
+        self.v_layout.addWidget(self.l1)
         
         # Menu deroulant _  Velib par arrondissement
         self.cb1 = QtGui.QComboBox()
-        self.cb1.addItems([unicode(str(i+1)) for i in range(20)])
+        self.cb1.addItems([str(i+1) for i in range(20)])
         self.cb1.setFixedWidth(300)
         self.cb1.currentIndexChanged.connect(self.velib_arrondissement)
-        self.layout.addWidget(self.cb1,2,0)
+        self.v_layout.addWidget(self.cb1)
         
          # Line Edit Resultats de recherche
         self.l2 = QtGui.QLabel()
         self.l2.setFixedWidth(300)
         self.l2.setText(u'Résultats')
-        self.layout.addWidget(self.l2,3,0)
+        self.v_layout.addWidget(self.l2)
         
         # Menu deroulant des resultats
         self.cb2 = QtGui.QComboBox()
         self.cb2.setFixedWidth(300)
-        self.layout.addWidget(self.cb2,4,0)
+        self.v_layout.addWidget(self.cb2)
         self.cb2.currentIndexChanged.connect(self.googlemap)
         
 #        # Menu deroulant du nombre de velibs disponibles
@@ -126,13 +134,12 @@ class SeeParisGui(QtGui.QWidget):
         # bouton -Lancer la recherche google map
         self.button3 = QtGui.QPushButton('Chercher', self)
         self.button3.setFixedWidth(150)
-        self.button3.clicked.connect(self.googlemap)
+        self.button3.clicked.connect(self.googlemap2)
         self.layout.addWidget(self.button3,8,1)
         
         # Histogramme bouton
         self.button4 = QtGui.QPushButton(u"Afficher l'histogramme")
         self.button4.setFixedWidth(300)
-#        self.button4.clicked.connect(self.plot_graph)
         self.button4.clicked.connect(self.histograph)
         self.layout.addWidget(self.button4,1,2)        
         
@@ -141,13 +148,78 @@ class SeeParisGui(QtGui.QWidget):
         self.details_url = "https://maps.googleapis.com/maps/api/place/details/json"
         
         #########################
+        # Donnees temps reel - lineEdit, Bouton et plot widget
+        # Bouton d'affichage 
+        self.button5 = QtGui.QPushButton(u"Affichage",self)
+        self.button5.clicked.connect(self.plot_graph)
+        self.layout.addWidget(self.button5,4,2)
         
-    # Methods
+        # Ligne de texte
+        self.l2 = QtGui.QLineEdit()
+        self.l2.setObjectName("arrondissement")
+        self.l2.setText(u"Numéro d'arrondissement")
+        self.layout.addWidget(self.l2,3,2)
         
+                
+        ############################ Methods ################################
     def plot_graph(self):
-        self.plotwidget.clear()
-        self.data = np.random.normal(loc=0.0,scale=2,size=100)
-        self.plotwidget.plot(self.data)
+        self.plotwidget2.clear()
+        x0,y0 = self.superhisto()
+        x0 = [(x0[i]-x0[0])/3600 for i in range(len(x0))]
+        self.data = pg.BarGraphItem(x=x0,height =y0,width=0.01)
+        self.plotwidget2.addItem(self.data)
+        self.plotwidget2.getAxis('bottom').setLabel('Temps (H)')
+        self.plotwidget2.getAxis('left').setLabel('Moyenne des velibs disponibles')
+    
+    def superhisto(self):
+        """ But : Trie les données d'acquisition par arrondissement
+        entrée : numero arrondissement
+        sortie : 2 vecteur tps,velo 
+            tps: temps d'acquisition
+            velo: nombre de vélo dans l'arrendissement pris au temps t
+        
+        NECESSAIRE : AVOIR LES DONNES D'ACQUISITION ET ADAPTER LE NOM DU
+        CHEMIN DES FICHIER 
+        """
+        numero = self.l2.text()
+        arr = int(numero)
+
+        if (arr <= 20):           
+
+            # Chemin d'acces et nom du fichier
+#            chemin_f = '/home/ak/fac/Projet python/acqui2/datta'
+            chemin_f = "C:/Users/Tiphaine/Documents/COURS/PHYSIQUE/M2_LuMI/S2/Projet_informatique/Day_data/datta"
+
+            k=1
+            Velo = []
+            Tps = []
+
+            while True :
+                try:
+                    #NOM DU FICHIER                        
+                        data =  open(chemin_f+str(k), 'r')
+                        Lecture_fichier = data.read()
+                        dico = dict(eval(Lecture_fichier))
+                        N = len(dico["records"])
+                            
+                        for i in range(N):
+                                if int(str(dico['records'][i]['fields']['name'][:2])) == int(arr):
+                                    
+                                    # nombre de vélo
+                                    nb_velo = dico['records'][i]['fields']["available_bikes"]
+                                    
+                                    #prendre l'heure et minute d'acquisition du fichier
+                                    #(renvoie a sa dete de creation)
+                                    a = os.stat(chemin_f+str(k))[8]
+                                    
+                                    Tps.append(a)
+                                    Velo.append(nb_velo)
+                                    k+=1
+                except:
+                         break
+            return Tps,Velo
+        else:
+            raise ValueError("""rentrez un numéro d'arrondissement valable""")
         
     def histograph(self):
         """ Trace le nombre de velibs disponibles en 'temps reel' a partir des
@@ -210,12 +282,9 @@ class SeeParisGui(QtGui.QWidget):
                      loc        = str(self.dvelib['records'][i]['fields']['address'])                    
                      velibdispo = self.dvelib['records'][i]['fields']['available_bikes']
                      info[loc]  = velibdispo
-                     self.cb2.addItem(unicode(loc))
-#                     self.cb3.blockSignals(True)
-#                     self.cb3.addItem(unicode(velibdispo))
-#                     self.cb3.blockSignals(False)
-#                     self.cb2.addItem(unicode(loc))
-#                     self.cb2.blockSignals(False)
+                     self.cb2.blockSignals(True)
+                     self.cb2.addItem(loc)
+                     self.cb2.blockSignals(False)
 #            print (info)
           except:
              print (u"Pas de station répertoriées dans cet arrondissement")
@@ -228,8 +297,33 @@ class SeeParisGui(QtGui.QWidget):
         entree: l'adresse d'une station velib
         sortie: l'url google map de cette adresse et ouverture d'une page internet
         """
-#        query = self.cb2.currentText()
+        query = self.cb2.currentText()
+#            query = str(self.l1.text())
+    
+        search_payload = {'key': key, 'query': query}
+        search_req = requests.get(self.search_url, params=search_payload )
+        search_json = search_req.json()
+        
+        place_id = search_json["results"][0]["place_id"]
+        
+        details_payload = {"key": key, "placeid": place_id}
+        details_resp = requests.get(self.details_url, params= details_payload)
+        details_json = details_resp.json()
+    
+        url = details_json["result"]["url"]
+        self.html2.load(QtCore.QUrl(url))
+        self.html2.show()
+    
+    def googlemap2(self):
+        """ La fonction construit un url avec lequel webbrowser ouvre la page
+        google map correspondante. Les donnees google map viennent du site Google
+        developers, mais il faut une cle.
+        
+        entree: l'adresse d'une station velib
+        sortie: l'url google map de cette adresse et ouverture d'une page internet
+        """
         query = str(self.l1.text())
+    
         search_payload = {'key': key, 'query': query}
         search_req = requests.get(self.search_url, params=search_payload )
         search_json = search_req.json()
